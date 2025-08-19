20589457b6f85978f76d9aff324ccc0906d91c64
Ishwar Dyade <ishwardyade.net@gmail.com>
2025-08-19 21:26:30 +0530

    error solved

--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '../../../../../../../lib/prisma';
+import { prisma } from '../../../../../../lib/prisma';
 
 // GET - Get specific user
 export async function GET(
